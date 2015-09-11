4432de214ecebd739954e528f2e936280591b46d
otchi <amineouchiha@hotmail.com>
2015-09-11 18:44:07 +0200

    add generique getting of JsonElement and add file functionnality access

--- src/main/java/com/edifixio/jsonFastBuild/selector/JsonHandleUtil.java
+++ src/main/java/com/edifixio/jsonFastBuild/selector/JsonHandleUtil.java
@@ -11,9 +11,10 @@ import java.util.List;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-
+@SuppressWarnings("unchecked")
 public abstract class JsonHandleUtil {
 	
 	public static final JsonParser JP=new JsonParser();
@@ -76,13 +77,13 @@ public abstract class JsonHandleUtil {
 		
 	}
 	
-	
+	/******************************************************************************/
 	public static JsonElement jsonFile(File file) throws FileNotFoundException, IOException{
 		return jsonBufferedReader(new BufferedReader(
 							new FileReader(file)));
 	}
 	
-	
+	/***********************************************************************/
 	public static JsonElement jsonBufferedReader(BufferedReader br) throws IOException{
 		String stringResult="",inter;
 		while((inter=br.readLine())!=null)
@@ -92,14 +93,43 @@ public abstract class JsonHandleUtil {
 		return JP.parse(stringResult);
 		
 	}
+
 	/******************************************************************************/
 	public static JsonElement jsonString(String jsonStr) {
 		return JP.parse(jsonStr);
 	}
 	
-	
-	public static void main(String args[]){	
+	/******************************************************************************/
+	public static <T extends JsonElement> T jsonString(Class<T> jsonType, String jsonStr) {
+		return (T) JP.parse(jsonStr);
 	}
+	/*********************************************************************************/
+	public static <T extends JsonElement> T jsonFile(Class<T> jsonType, String path) throws IOException {
+
+		return (T) jsonBufferedReader(new BufferedReader(new FileReader(new File(path))));
+
+	}
+	/***********************************************************************************/
+	public static <T extends JsonElement> T jsonFile(Class<T> jsonType, File file)
+			throws FileNotFoundException, IOException {
+		return (T) jsonBufferedReader(new BufferedReader(new FileReader(file)));
+	}
+	/******************************************************************************************/
+	public static <T extends JsonElement> T jsonFile(Class<T> jsonType, BufferedReader br) throws IOException {
+		String stringResult = "", inter;
+		while ((inter = br.readLine()) != null)
+			stringResult += inter;
+
+		br.close();
+		return (T) JP.parse(stringResult);
+
+	}
+	
+	/*public static void main(String args[]) throws IOException{
+		
+		JsonObject jo=jsonString(JsonObject.class,"{query:{match_all:{}}}");
+		System.out.println(jo);
+	}*/
 
 	
 	
